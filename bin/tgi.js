9055534d47031593e331298eca773a137d4d2177
Sean Smith <sean@tgicloud.com>
2015-01-03 09:32:25 -0500

    revisit after core changes

--- bin/tgi.js
+++ bin/tgi.js
@@ -5,17 +5,30 @@
 /**
  * Node modules
  */
-var tgicore = require('tgi-core');
+var tgicore = require('../node_modules/tgi-core/dist/tgi.coreUtil.js');
+//var tgiutility = require('tgi-utility');
 var appInit = require('../lib/app-init');
 var cliServer = require('../lib/cli-server');
 
 /**
  * Here is our App for tgi
  */
-var app = {};
-if (appInit(app)) {
-  //console.log('tgi ' + app.version);
-  if (app.startREPL) {
-    cliServer();
+(function () {
+  var tgi = {};
+  //tgiutility().injectMethods(tgi);
+  tgicore().injectMethods(tgi);
+  var getInvalidProperties = tgi.getInvalidProperties;
+  var fuck = new tgi.Model();
+  var app = new tgi.Application();
+  app.setInterface(new tgi.Interface());
+  app.start(function () {
+
+  });
+  if (appInit(app)) {
+    //console.log('tgi ' + app.version);
+    if (app.startREPL) {
+      cliServer();
+    }
   }
-}
+}());
+
