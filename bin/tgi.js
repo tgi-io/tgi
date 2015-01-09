3da626441927378f49b5f58c28dc7dec536eb4c9
Sean Smith <sean@tgicloud.com>
2015-01-09 07:08:17 -0500

    revisit after interface-repl created

--- bin/tgi.js
+++ bin/tgi.js
@@ -5,30 +5,28 @@
 /**
  * Node modules
  */
-var tgicore = require('../node_modules/tgi-core/dist/tgi.coreUtil.js');
-//var tgiutility = require('tgi-utility');
+var tgicore = require('../node_modules/tgi-core/dist/tgi.core.js');
 var appInit = require('../lib/app-init');
 var cliServer = require('../lib/cli-server');
 
 /**
  * Here is our App for tgi
  */
-(function () {
-  var tgi = {};
-  //tgiutility().injectMethods(tgi);
-  tgicore().injectMethods(tgi);
-  var getInvalidProperties = tgi.getInvalidProperties;
-  var fuck = new tgi.Model();
-  var app = new tgi.Application();
-  app.setInterface(new tgi.Interface());
-  app.start(function () {
-
-  });
-  if (appInit(app)) {
-    //console.log('tgi ' + app.version);
-    if (app.startREPL) {
-      cliServer();
-    }
+var tgi = tgicore();
+var ui = new tgi.Interface();
+var app = new tgi.Application();
+app.setInterface(ui);
+app.start(function (shizzle) {
+  //console.log(JSON.stringify(shizzle));
+});
+if (appInit(app)) {
+  if (app.startREPL) {
+    cliServer();
   }
-}());
+}
+
+var cmd = new tgi.Command(function () {
+  console.log('here be cmd');
+});
+ui.mockRequest(new tgi.Request({type: 'Command', command: cmd}));
 
